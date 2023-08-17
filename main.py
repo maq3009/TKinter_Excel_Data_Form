44d442cdabad53db5f90e5f9b08d94c731a76f74
Misael <maq3009@hotmail.com>
2023-08-16 22:46:46 -0400

    added equipment frame and equipment name label

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("Boiler Room Inventory App")
+
+frame = tkinter.Frame(window)
+frame.pack()
+
+# Saving Equipment Information
+equipment_info_frame = tkinter.LabelFrame(frame, text = "Equipment Info")
+equipment_info_frame.grid(row = 0, column = 0)
+
+equipment_name_label = tkinter.Label(equipment_info_frame, text = "Equipment Name")
+equipment_name_label.grid(row = 0, column = 0)
 
 
 window.mainloop()
